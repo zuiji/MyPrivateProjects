554a8db48f1a52b1874e0936669df1d625f6a75f
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2018-12-13 21:40:58 +0100

    done and added some Text

--- Fundamentals of Programming Understanding C#/Using the Char Data Type/Using the Char Data Type/Program.cs
+++ Fundamentals of Programming Understanding C#/Using the Char Data Type/Using the Char Data Type/Program.cs
@@ -8,8 +8,13 @@ namespace Using_the_Char_Data_Type
 {
     class Program
     {
+        // Lecture 6: Using the Char Data Type
         static void Main(string[] args)
         {
+            //Using \ to close so i can display ' as the char.
+            char myChar = '\'';
+            Console.WriteLine(myChar);
+
         }
     }
 }
