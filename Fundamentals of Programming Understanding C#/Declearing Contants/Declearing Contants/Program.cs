1dff53518919c8232e4580de5796ee7dd7044d74
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2018-12-13 22:30:18 +0100

    Learnend you can never change a Constant like math.pi and you can make new virabels with an enum that locks the items so they cant be changed without change in the codee

--- Fundamentals of Programming Understanding C#/Declearing Contants/Declearing Contants/Program.cs
+++ Fundamentals of Programming Understanding C#/Declearing Contants/Declearing Contants/Program.cs
@@ -8,9 +8,33 @@ namespace Declearing_Contants
 {
     class Program
     {
+
+
+        enum DaysOfWeek
+    {
+        SUNDAY, 
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY
+    }
+   
         //Lecture 9: Declaring Constants
         static void Main(string[] args)
         {
+            //Cpmst you can never change a const.
+            const int myConstInt = 5;
+
+            //myConstInt = 6;
+            Console.WriteLine(Math.PI);
+            DaysOfWeek today = DaysOfWeek.FRIDAY;
+            Console.WriteLine(today);
+
+            int numberOfToday = (int) today;
+            Console.WriteLine(numberOfToday);
+
 
         }
     }
