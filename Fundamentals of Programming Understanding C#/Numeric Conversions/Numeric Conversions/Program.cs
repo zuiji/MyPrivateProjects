a932a8ede337d0d4222babe6ee24cf00501e0f05
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2018-12-13 22:18:56 +0100

    learned abit about Casting.
    like you can push a double into an int my do (int)YourVariablesName; but you will lose some data as the Int cant hold ex 5.5 so its will only be to 5 then you type it out to the console.
    
    but you can go from an int to a double with no problems.

--- Fundamentals of Programming Understanding C#/Numeric Conversions/Numeric Conversions/Program.cs
+++ Fundamentals of Programming Understanding C#/Numeric Conversions/Numeric Conversions/Program.cs
@@ -12,8 +12,14 @@ namespace Numeric_Conversions
         static void Main(string[] args)
         {
             int x = 5;
-
             double y = x;
+
+            double myDouble = 5.5;
+
+            //to force a double to an int you use (int)YourVariablesName; but you will lose some data!!!!! 
+            // do what every you need to, to push it into the int. 
+            int myInt = (int)myDouble;
+            Console.WriteLine(myInt);
 
 
         }
