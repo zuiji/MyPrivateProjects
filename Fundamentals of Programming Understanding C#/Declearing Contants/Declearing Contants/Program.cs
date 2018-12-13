81fa7e126c22add16b961ae6cfbebaea3b1e0b7c
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2018-12-13 22:20:39 +0100

    //Lecture 9: Declaring Constants

---
+++ Fundamentals of Programming Understanding C#/Declearing Contants/Declearing Contants/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Declearing_Contants
+{
+    class Program
+    {
+        //Lecture 9: Declaring Constants
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
