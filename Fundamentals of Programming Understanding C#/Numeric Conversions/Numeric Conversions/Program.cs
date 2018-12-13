acc59b328f44c355a0afa142f1a3a75b32cd8c86
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2018-12-13 22:12:23 +0100

    //Lecture 8: Numeric Conversions

---
+++ Fundamentals of Programming Understanding C#/Numeric Conversions/Numeric Conversions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeric_Conversions
+{
+    class Program
+    {
+        //Lecture 8: Numeric Conversions
+        static void Main(string[] args)
+        {
+            int x = 5;
+
+            double y = x;
+
+
+        }
+    }
+}
