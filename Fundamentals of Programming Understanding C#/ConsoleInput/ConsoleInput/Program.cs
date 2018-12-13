1643b1d2959edaa8202cc81f287fb14f9f232157
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2018-12-13 22:41:26 +0100

    learned abit of how to convote a string to an int with a "int.Parse"

--- Fundamentals of Programming Understanding C#/ConsoleInput/ConsoleInput/Program.cs
+++ Fundamentals of Programming Understanding C#/ConsoleInput/ConsoleInput/Program.cs
@@ -11,7 +11,21 @@ namespace ConsoleInput
         //Lecture 10: Console Input
 
         static void Main(string[] args)
-        {
+        { 
+            //    Console.WriteLine("What is your name?");
+            //    string name = Console.ReadLine();
+            //    Console.WriteLine("What is your lastname");
+            //    string lastname = Console.ReadLine();
+            //    Console.WriteLine("Hello {0} {1} ", name, lastname);
+
+
+
+            // how to read as a number instead of a string
+
+            Console.WriteLine("Enter a Number");
+            int number = int.Parse(Console.ReadLine());
+            number += 10;
+            Console.WriteLine("The new number is {0}", number);
 
         }
     }
