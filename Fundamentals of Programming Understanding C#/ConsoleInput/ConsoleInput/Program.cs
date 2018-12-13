cf10345b9f6ae6a7ef2668898f5ff7f0a3d3ab8a
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2018-12-13 22:32:07 +0100

    //Lecture 10: Console Input

---
+++ Fundamentals of Programming Understanding C#/ConsoleInput/ConsoleInput/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleInput
+{
+    class Program
+    {
+        //Lecture 10: Console Input
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
