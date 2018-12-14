9b05909e672abff4bb2cf476c8919df1094c553c
Peter Bøgh Stubberup <pete646a@zbc.dk>
2018-12-14 09:48:49 +0100

    Starting up the Exercise 2 in Using Data

---
+++ Fundamentals of Programming Understanding C#/Exercise2 Using Data/Exercise2 Using Data/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2_Using_Data
+{
+    class Program
+    {
+        // Exercise 2
+        /*
+         * Write a Program that asks the user for the length and width of there room. Fefine a constant
+         * that represents the price of carpeting per square foot.
+         * compute and display the cost of carpeting the room.
+         *
+         *
+         *
+         */
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
