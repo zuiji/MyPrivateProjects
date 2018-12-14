8a623be67b38a2baa5ec6e08a6349228502c5f7e
Peter Bøgh Stubberup <pete646a@zbc.dk>
2018-12-14 09:35:59 +0100

    done with first Exercise

---
+++ Fundamentals of Programming Understanding C#/Exercise 1 Using Data/Exercise 1 Using Data/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1_Using_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The first number");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int answer = firstNumber + secondNumber;
+
+            Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, answer);
+        }
+    }
+}
