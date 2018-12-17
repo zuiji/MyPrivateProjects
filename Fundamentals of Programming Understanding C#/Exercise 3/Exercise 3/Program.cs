e476aed3519d0f5daca3c123b6d006b7475a2328
Peter Bøgh Stubberup <pete646a@zbc.dk>
2018-12-17 10:20:24 +0100

    Done with exercise 3

--- Fundamentals of Programming Understanding C#/Exercise 3/Exercise 3/Program.cs
+++ Fundamentals of Programming Understanding C#/Exercise 3/Exercise 3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,7 +22,12 @@ namespace Exercise_3
          */
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the total amount of time in Minuts only");
+            int totalTimeInMinuts = int.Parse(Console.ReadLine());
+            int hours = totalTimeInMinuts / 60;
+            int remainingMintes = totalTimeInMinuts % 60;
 
+            Console.WriteLine("{0} minutes is equal to {1} hours and {2} minutes", totalTimeInMinuts, hours, remainingMintes);
         }
     }
 }
