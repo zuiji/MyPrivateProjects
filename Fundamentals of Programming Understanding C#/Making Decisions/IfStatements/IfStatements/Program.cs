5725cb99e772195bec0d87e0b9d624690b1bdc7a
Peter Bøgh Stubberup <pete646a@zbc.dk>
2018-12-17 11:05:06 +0100

    Learned you can have multiplay if's insite each other and they work like a Bool with its only run the statment if its true. and if its false its not.

--- Fundamentals of Programming Understanding C#/Making Decisions/IfStatements/IfStatements/Program.cs
+++ Fundamentals of Programming Understanding C#/Making Decisions/IfStatements/IfStatements/Program.cs
@@ -12,7 +12,16 @@ namespace IfStatements
         //Lecture 15: If Statements
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter your age");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 18)
+            {
+                if (age <= 55)
+                {
+                    Console.WriteLine("You're good to go");
+                }
 
+            }
         }
     }
 }
