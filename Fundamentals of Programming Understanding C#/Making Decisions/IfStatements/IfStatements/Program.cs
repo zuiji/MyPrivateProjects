86877781f8d17305c407c33e95743c755c1cf61b
Peter Bøgh Stubberup <pete646a@zbc.dk>
2018-12-17 10:42:04 +0100

    Starting up Lecture 15 : If Statements

---
+++ Fundamentals of Programming Understanding C#/Making Decisions/IfStatements/IfStatements/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatements
+{
+    class Program
+    {
+
+        //Lecture 15: If Statements
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
