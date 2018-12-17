b2ddf4374449e1da79123f443a4fd2776ed7c484
Peter Bøgh Stubberup <pete646a@zbc.dk>
2018-12-17 10:12:54 +0100

    Starting on Exersice 3

---
+++ Fundamentals of Programming Understanding C#/Exercise 3/Exercise 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    class Program
+    {
+
+        //Exercise 3
+        /*
+         *
+         * Write a program that asks the user to enter a total amount of time in minutes only.
+         * Print to the console the same amount of time in hours and minutes.
+         *
+         *
+         *
+         *
+         */
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
