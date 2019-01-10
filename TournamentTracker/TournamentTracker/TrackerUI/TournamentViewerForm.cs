6a05f7d87536b21465a07ec168f953db15cc0594
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-01-10 21:18:00 +0100

    Starting up on C# App Start To Finish with IAmTimCorey

--- TournamentTracker/TournamentTracker/TrackerUI/TournamentViewerForm.cs
+++ TournamentTracker/TournamentTracker/TrackerUI/TournamentViewerForm.cs
@@ -16,5 +16,10 @@ namespace TrackerUI
         {
             InitializeComponent();
         }
+
+        private void roundLabel_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
