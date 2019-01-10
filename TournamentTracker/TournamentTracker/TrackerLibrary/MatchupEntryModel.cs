17b75ccc945b2bd1c0b4cabb526eee1422943e83
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-01-10 20:25:51 +0100

    Starting up on C# App Start To Finish With IAmTimCorey

---
+++ TournamentTracker/TournamentTracker/TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represents one team in the match up.
+        /// </summary>
+
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represents the Score for the Particular team.
+        /// </summary>
+
+        public double Score { get; set; }
+
+        /// <summary>
+        /// Represents the match up that this team come from as the winner.
+        /// </summary>
+
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
