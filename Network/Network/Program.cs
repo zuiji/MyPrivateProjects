d93dc2bafa0eb386954e7c11ad844141939ea9e1
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-03-28 09:59:36 +0100

    Uploading static to static examble

---
+++ Network/Network/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            IPAddress ip = new IPAddress(new byte[] { 192, 168, 1, 1 });
+            int bits = 25;
+
+            uint mask = ~(uint.MaxValue >> bits);
+
+            // Convert the IP address to bytes.
+            byte[] ipBytes = ip.GetAddressBytes();
+
+            // BitConverter gives bytes in opposite order to GetAddressBytes().
+            byte[] maskBytes = BitConverter.GetBytes(mask).Reverse().ToArray();
+
+            byte[] startIPBytes = new byte[ipBytes.Length];
+            byte[] endIPBytes = new byte[ipBytes.Length];
+
+            // Calculate the bytes of the start and end IP addresses.
+            for (int i = 0; i < ipBytes.Length; i++)
+            {
+                startIPBytes[i] = (byte)(ipBytes[i] & maskBytes[i]);
+                endIPBytes[i] = (byte)(ipBytes[i] | ~maskBytes[i]);
+            }
+
+            // Convert the bytes to IP addresses.
+            IPAddress startIP = new IPAddress(startIPBytes);
+            IPAddress endIP = new IPAddress(endIPBytes);
+            
+            Console.WriteLine(startIP);
+            Console.WriteLine(endIP);
+        }
+    }
+}
