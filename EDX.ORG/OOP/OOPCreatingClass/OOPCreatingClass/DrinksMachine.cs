129f770e589d2563336014f1e84a586b87cda3ac
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-04-11 10:50:55 +0200

    ...

---
+++ EDX.ORG/OOP/OOPCreatingClass/OOPCreatingClass/DrinksMachine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPCreatingClass
+{
+    class DrinksMachine
+    {
+        // The following statements declare private member variables
+        public string _location;
+        public string _make;
+        public string _model;
+
+
+        // The following statements declare public methods
+        public void MakeCappuccino()
+        {
+            // Method logic goes here
+            Console.WriteLine("Cappuccino is made.");
+        }
+    }
+}
---
+++ EDX.ORG/OOP/OOPCreatingClass/OOPCreatingClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPCreatingClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrinksMachine myMachine = new DrinksMachine();
+            myMachine._location = "kitchen";
+            myMachine._model = "DM1000";
+
+            Console.WriteLine(myMachine._location);
+            Console.WriteLine(myMachine._model);
+
+            myMachine.MakeCappuccino();
+            
+        }
+    }
+}
