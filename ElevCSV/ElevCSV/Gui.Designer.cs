98cfea889864f3c96d760f0624d8bb3d876b8b90
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-06-18 16:06:45 +0200

    Added Gui

---
+++ ElevCSV/ElevCSV/Gui.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace ElevCSV
+{
+    partial class Gui
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CPRNumberLabel = new System.Windows.Forms.Label();
+            this.PhoneNumberLabel = new System.Windows.Forms.Label();
+            this.WhereDidYouTakeYourGFLabel = new System.Windows.Forms.Label();
+            this.DirectionLabel = new System.Windows.Forms.Label();
+            this.EuxLabel = new System.Windows.Forms.Label();
+            this.WantedSkpLocationLabel = new System.Windows.Forms.Label();
+            this.GeneralInfoMationLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.PhoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.CPRNumberTextBox = new System.Windows.Forms.TextBox();
+            this.DirectionComboBox = new System.Windows.Forms.ComboBox();
+            this.WhereDidYouTakeYourGF2ComboBox = new System.Windows.Forms.ComboBox();
+            this.WantedSkpLocationComboBox = new System.Windows.Forms.ComboBox();
+            this.EuxComboBox = new System.Windows.Forms.ComboBox();
+            this.FirstNameLabel = new System.Windows.Forms.Label();
+            this.MiddleNameLabel = new System.Windows.Forms.Label();
+            this.LastNameLabel = new System.Windows.Forms.Label();
+            this.EmailLabel = new System.Windows.Forms.Label();
+            this.FirstnameTextBox = new System.Windows.Forms.TextBox();
+            this.LastnameTextBox = new System.Windows.Forms.TextBox();
+            this.MiddlenameTextBox = new System.Windows.Forms.TextBox();
+            this.EmailTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CPRNumberLabel
+            // 
+            this.CPRNumberLabel.AutoSize = true;
+            this.CPRNumberLabel.Location = new System.Drawing.Point(266, 68);
+            this.CPRNumberLabel.Name = "CPRNumberLabel";
+            this.CPRNumberLabel.Size = new System.Drawing.Size(71, 13);
+            this.CPRNumberLabel.TabIndex = 3;
+            this.CPRNumberLabel.Text = "CPR-Nummer";
+            // 
+            // PhoneNumberLabel
+            // 
+            this.PhoneNumberLabel.AutoSize = true;
+            this.PhoneNumberLabel.Location = new System.Drawing.Point(260, 22);
+            this.PhoneNumberLabel.Name = "PhoneNumberLabel";
+            this.PhoneNumberLabel.Size = new System.Drawing.Size(83, 13);
+            this.PhoneNumberLabel.TabIndex = 5;
+            this.PhoneNumberLabel.Text = "Telefon nummer";
+            // 
+            // WhereDidYouTakeYourGFLabel
+            // 
+            this.WhereDidYouTakeYourGFLabel.AutoSize = true;
+            this.WhereDidYouTakeYourGFLabel.Location = new System.Drawing.Point(440, 69);
+            this.WhereDidYouTakeYourGFLabel.Name = "WhereDidYouTakeYourGFLabel";
+            this.WhereDidYouTakeYourGFLabel.Size = new System.Drawing.Size(100, 13);
+            this.WhereDidYouTakeYourGFLabel.TabIndex = 6;
+            this.WhereDidYouTakeYourGFLabel.Text = "Hvor tog du dit GF2";
+            // 
+            // DirectionLabel
+            // 
+            this.DirectionLabel.AutoSize = true;
+            this.DirectionLabel.Location = new System.Drawing.Point(468, 23);
+            this.DirectionLabel.Name = "DirectionLabel";
+            this.DirectionLabel.Size = new System.Drawing.Size(44, 13);
+            this.DirectionLabel.TabIndex = 7;
+            this.DirectionLabel.Text = "Retning";
+            // 
+            // EuxLabel
+            // 
+            this.EuxLabel.AutoSize = true;
+            this.EuxLabel.Location = new System.Drawing.Point(287, 113);
+            this.EuxLabel.Name = "EuxLabel";
+            this.EuxLabel.Size = new System.Drawing.Size(29, 13);
+            this.EuxLabel.TabIndex = 8;
+            this.EuxLabel.Text = "EUX";
+            // 
+            // WantedSkpLocationLabel
+            // 
+            this.WantedSkpLocationLabel.AutoSize = true;
+            this.WantedSkpLocationLabel.Location = new System.Drawing.Point(436, 115);
+            this.WantedSkpLocationLabel.Name = "WantedSkpLocationLabel";
+            this.WantedSkpLocationLabel.Size = new System.Drawing.Size(109, 13);
+            this.WantedSkpLocationLabel.TabIndex = 9;
+            this.WantedSkpLocationLabel.Text = "Ønsket SKP Lokation";
+            // 
+            // GeneralInfoMationLabel
+            // 
+            this.GeneralInfoMationLabel.AutoSize = true;
+            this.GeneralInfoMationLabel.Location = new System.Drawing.Point(364, 167);
+            this.GeneralInfoMationLabel.Name = "GeneralInfoMationLabel";
+            this.GeneralInfoMationLabel.Size = new System.Drawing.Size(64, 13);
+            this.GeneralInfoMationLabel.TabIndex = 10;
+            this.GeneralInfoMationLabel.Text = "Særlige Info";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(231, 183);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(320, 194);
+            this.textBox1.TabIndex = 11;
+            // 
+            // PhoneNumberTextBox
+            // 
+            this.PhoneNumberTextBox.Location = new System.Drawing.Point(241, 42);
+            this.PhoneNumberTextBox.Name = "PhoneNumberTextBox";
+            this.PhoneNumberTextBox.Size = new System.Drawing.Size(121, 20);
+            this.PhoneNumberTextBox.TabIndex = 15;
+            // 
+            // CPRNumberTextBox
+            // 
+            this.CPRNumberTextBox.Location = new System.Drawing.Point(241, 87);
+            this.CPRNumberTextBox.Name = "CPRNumberTextBox";
+            this.CPRNumberTextBox.Size = new System.Drawing.Size(121, 20);
+            this.CPRNumberTextBox.TabIndex = 17;
+            // 
+            // DirectionComboBox
+            // 
+            this.DirectionComboBox.AllowDrop = true;
+            this.DirectionComboBox.FormattingEnabled = true;
+            this.DirectionComboBox.Location = new System.Drawing.Point(430, 42);
+            this.DirectionComboBox.Name = "DirectionComboBox";
+            this.DirectionComboBox.Size = new System.Drawing.Size(121, 21);
+            this.DirectionComboBox.TabIndex = 18;
+            // 
+            // WhereDidYouTakeYourGF2ComboBox
+            // 
+            this.WhereDidYouTakeYourGF2ComboBox.AllowDrop = true;
+            this.WhereDidYouTakeYourGF2ComboBox.FormattingEnabled = true;
+            this.WhereDidYouTakeYourGF2ComboBox.Location = new System.Drawing.Point(430, 88);
+            this.WhereDidYouTakeYourGF2ComboBox.Name = "WhereDidYouTakeYourGF2ComboBox";
+            this.WhereDidYouTakeYourGF2ComboBox.Size = new System.Drawing.Size(121, 21);
+            this.WhereDidYouTakeYourGF2ComboBox.TabIndex = 19;
+            // 
+            // WantedSkpLocationComboBox
+            // 
+            this.WantedSkpLocationComboBox.AllowDrop = true;
+            this.WantedSkpLocationComboBox.FormattingEnabled = true;
+            this.WantedSkpLocationComboBox.Location = new System.Drawing.Point(430, 134);
+            this.WantedSkpLocationComboBox.Name = "WantedSkpLocationComboBox";
+            this.WantedSkpLocationComboBox.Size = new System.Drawing.Size(121, 21);
+            this.WantedSkpLocationComboBox.TabIndex = 20;
+            // 
+            // EuxComboBox
+            // 
+            this.EuxComboBox.AllowDrop = true;
+            this.EuxComboBox.FormattingEnabled = true;
+            this.EuxComboBox.Location = new System.Drawing.Point(241, 133);
+            this.EuxComboBox.Name = "EuxComboBox";
+            this.EuxComboBox.Size = new System.Drawing.Size(121, 21);
+            this.EuxComboBox.TabIndex = 21;
+            // 
+            // FirstNameLabel
+            // 
+            this.FirstNameLabel.AutoSize = true;
+            this.FirstNameLabel.Location = new System.Drawing.Point(93, 22);
+            this.FirstNameLabel.Name = "FirstNameLabel";
+            this.FirstNameLabel.Size = new System.Drawing.Size(46, 13);
+            this.FirstNameLabel.TabIndex = 0;
+            this.FirstNameLabel.Text = "Fornavn";
+            // 
+            // MiddleNameLabel
+            // 
+            this.MiddleNameLabel.AutoSize = true;
+            this.MiddleNameLabel.Location = new System.Drawing.Point(84, 69);
+            this.MiddleNameLabel.Name = "MiddleNameLabel";
+            this.MiddleNameLabel.Size = new System.Drawing.Size(64, 13);
+            this.MiddleNameLabel.TabIndex = 1;
+            this.MiddleNameLabel.Text = "Mellemnavn";
+            // 
+            // LastNameLabel
+            // 
+            this.LastNameLabel.AutoSize = true;
+            this.LastNameLabel.Location = new System.Drawing.Point(90, 116);
+            this.LastNameLabel.Name = "LastNameLabel";
+            this.LastNameLabel.Size = new System.Drawing.Size(53, 13);
+            this.LastNameLabel.TabIndex = 2;
+            this.LastNameLabel.Text = "Efternavn";
+            // 
+            // EmailLabel
+            // 
+            this.EmailLabel.AutoSize = true;
+            this.EmailLabel.Location = new System.Drawing.Point(100, 163);
+            this.EmailLabel.Name = "EmailLabel";
+            this.EmailLabel.Size = new System.Drawing.Size(32, 13);
+            this.EmailLabel.TabIndex = 4;
+            this.EmailLabel.Text = "Email";
+            // 
+            // FirstnameTextBox
+            // 
+            this.FirstnameTextBox.Location = new System.Drawing.Point(28, 42);
+            this.FirstnameTextBox.Name = "FirstnameTextBox";
+            this.FirstnameTextBox.Size = new System.Drawing.Size(176, 20);
+            this.FirstnameTextBox.TabIndex = 12;
+            // 
+            // LastnameTextBox
+            // 
+            this.LastnameTextBox.Location = new System.Drawing.Point(28, 136);
+            this.LastnameTextBox.Name = "LastnameTextBox";
+            this.LastnameTextBox.Size = new System.Drawing.Size(176, 20);
+            this.LastnameTextBox.TabIndex = 13;
+            // 
+            // MiddlenameTextBox
+            // 
+            this.MiddlenameTextBox.Location = new System.Drawing.Point(28, 89);
+            this.MiddlenameTextBox.Name = "MiddlenameTextBox";
+            this.MiddlenameTextBox.Size = new System.Drawing.Size(176, 20);
+            this.MiddlenameTextBox.TabIndex = 14;
+            // 
+            // EmailTextBox
+            // 
+            this.EmailTextBox.Location = new System.Drawing.Point(28, 183);
+            this.EmailTextBox.Name = "EmailTextBox";
+            this.EmailTextBox.Size = new System.Drawing.Size(176, 20);
+            this.EmailTextBox.TabIndex = 16;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ElevCSV.ZBClogo.ZBCLogo;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 230);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(176, 141);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Gui
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 383);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.EuxComboBox);
+            this.Controls.Add(this.WantedSkpLocationComboBox);
+            this.Controls.Add(this.WhereDidYouTakeYourGF2ComboBox);
+            this.Controls.Add(this.DirectionComboBox);
+            this.Controls.Add(this.CPRNumberTextBox);
+            this.Controls.Add(this.EmailTextBox);
+            this.Controls.Add(this.PhoneNumberTextBox);
+            this.Controls.Add(this.MiddlenameTextBox);
+            this.Controls.Add(this.LastnameTextBox);
+            this.Controls.Add(this.FirstnameTextBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.GeneralInfoMationLabel);
+            this.Controls.Add(this.WantedSkpLocationLabel);
+            this.Controls.Add(this.EuxLabel);
+            this.Controls.Add(this.DirectionLabel);
+            this.Controls.Add(this.WhereDidYouTakeYourGFLabel);
+            this.Controls.Add(this.PhoneNumberLabel);
+            this.Controls.Add(this.EmailLabel);
+            this.Controls.Add(this.CPRNumberLabel);
+            this.Controls.Add(this.LastNameLabel);
+            this.Controls.Add(this.MiddleNameLabel);
+            this.Controls.Add(this.FirstNameLabel);
+            this.Name = "Gui";
+            this.Text = "Gui";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label CPRNumberLabel;
+        private System.Windows.Forms.Label PhoneNumberLabel;
+        private System.Windows.Forms.Label WhereDidYouTakeYourGFLabel;
+        private System.Windows.Forms.Label DirectionLabel;
+        private System.Windows.Forms.Label EuxLabel;
+        private System.Windows.Forms.Label WantedSkpLocationLabel;
+        private System.Windows.Forms.Label GeneralInfoMationLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox PhoneNumberTextBox;
+        private System.Windows.Forms.TextBox CPRNumberTextBox;
+        private System.Windows.Forms.ComboBox DirectionComboBox;
+        private System.Windows.Forms.ComboBox WhereDidYouTakeYourGF2ComboBox;
+        private System.Windows.Forms.ComboBox WantedSkpLocationComboBox;
+        private System.Windows.Forms.ComboBox EuxComboBox;
+        private System.Windows.Forms.Label FirstNameLabel;
+        private System.Windows.Forms.Label MiddleNameLabel;
+        private System.Windows.Forms.Label LastNameLabel;
+        private System.Windows.Forms.Label EmailLabel;
+        private System.Windows.Forms.TextBox FirstnameTextBox;
+        private System.Windows.Forms.TextBox LastnameTextBox;
+        private System.Windows.Forms.TextBox MiddlenameTextBox;
+        private System.Windows.Forms.TextBox EmailTextBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
